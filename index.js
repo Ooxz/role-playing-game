64e26ee45633b1b43f79b055583a6787fd03e35e
Brune <abrune.osteo@gmail.com>
2022-10-12 18:58:46 +0200

    js update

--- index.js
+++ index.js
@@ -1,51 +1,19 @@
-const hero = {
-    elementId: "hero", 
-    name: "Wizard", 
-    avatar: "images/wizard.png", 
-    health: 60, 
-	diceCount: 3,
+import characterData from "./data.js"
+import Character from "./character.js"
+
+const wizard = new Character(characterData.hero);
+const orc = new Character(characterData.monster);
+
+function render() {
+	document.getElementById("hero").innerHTML = wizard.getCharacterHtml()
+	document.getElementById("monster").innerHTML = orc.getCharacterHtml()
 }
 
-const monster = {
-    elementId: "monster", 
-    name: "Orc", 
-    avatar: "images/orc.png", 
-    health: 10, 
-	diceCount: 1,
+render()
+
+document.getElementById("attack-button").addEventListener("click", attack)
+
+function attack() {
+	console.log("attack button is working")
 }
-
-function getDiceRollArray(diceCount) {
-	return new Array(diceCount).fill(0).map(function() {
-		return (Math.floor(Math.random() * 6) + 1)
-	})
-}
-
-function getDiceHtml(diceCount) {
-	return getDiceRollArray(diceCount).map(function(num) {
-		return `<div class="dice">${num}</div>`
-	})
-	
-}
-
-
-
-function renderCharacter(data) {
-	const {elementId, name, avatar, health, diceCount} = data
-	const diceHtml = getDiceHtml(diceCount).join('')
-
-	document.getElementById(elementId).innerHTML = `
-<div class="character-card">
-<h4 class="name">${name}</h4>
-<img class="avatar" src="${avatar}"/>
-<p class="health">health: <b>${health} </b></p>
-<div class="dice-container">
-${diceHtml}
-</div>
-`
-}
-
-renderCharacter(hero)
-renderCharacter(monster)
-
-
 
