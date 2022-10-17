332856b62ebe0bca651ffbc00b82d6405464766c
Brune <abrune.osteo@gmail.com>
2022-10-17 14:45:31 +0200

    js update class constructor

--- character.js
+++ character.js
@@ -1,18 +1,21 @@
 import { getDiceRollArray, getDicePlaceholderHtml, getPercentage } from './utils.js'
 
-function Character(data) {
-    Object.assign(this, data)
-    this.maxHealth = this.health
+class Character {
+    constructor(data) {
+        Object.assign(this, data)
+        
+        this.maxHealth = this.health
 
-    this.diceArray = getDicePlaceholderHtml(this.diceCount)
+        this.diceHtml = getDicePlaceholderHtml(this.diceCount)
 
-    this.getDiceHtml = function () {
+       
+    }
+    setDiceHtml() {
         this.currentDiceScore = getDiceRollArray(this.diceCount)
-        this.diceArray = this.currentDiceScore.map((num) =>
-            `<div class="dice">${num}</div>`).join("")
+        this.diceHtml = this.currentDiceScore.map((num) => `<div class="dice">${num}</div>`).join("")
     }
 
-    this.takeDamage = function (attackScoreArray) {
+    takeDamage(attackScoreArray) {
         const totalAttackScore = attackScoreArray.reduce((total, num) => total + num)
         this.health -= totalAttackScore
         if (this.health <= 0) {
@@ -22,29 +25,29 @@ function Character(data) {
     }
 
 
-    this.getHealthBarHtml = function () {
+    getHealthBarHtml() {
         const percent = getPercentage(this.health, this.maxHealth)
         return `<div class="health-bar-outer">
-                    <div class="health-bar-inner ${percent < 26 ? "danger" : ""}" 
-                            style="width:${percent}%;">
-                    </div>
-                </div>`  
-    }
-    
-
-    this.getCharacterHtml = function () {
-        const { elementId, name, avatar, health, diceCount } = this
-        const healthBar = this.getHealthBarHtml()
-        return `
-            <div class="character-card">
-                <h4 class="name"> ${name} </h4>
-                <img class="avatar" src="${avatar}" />
-                <div class="health">health: <b> ${health} </b></div>
-                ${healthBar}
-                <div class="dice-container">
-                    ${this.diceArray}
+                <div class="health-bar-inner ${percent < 26 ? "danger" : ""}" 
+                        style="width:${percent}%;">
                 </div>
             </div>`
+    }
+
+
+    getCharacterHtml() {
+        const { elementId, name, avatar, health, diceCount, diceHtml } = this
+        const healthBar = this.getHealthBarHtml()
+        return `
+        <div class="character-card">
+            <h4 class="name"> ${name} </h4>
+            <img class="avatar" src="${avatar}" />
+            <div class="health">health: <b> ${health} </b></div>
+            ${healthBar}
+            <div class="dice-container">
+                ${diceHtml}
+            </div>
+        </div>`
     }
 }
 
--- index.js
+++ index.js
@@ -1,15 +1,8 @@
 import characterData from './data.js'
 import Character from './Character.js'
 
-/*
-Challenge
-1. See if you can get the app to work with just 
-one monster again.
-**hint.md for help!!**
-*/
-
-
 let monstersArray = ["orc", "demon", "goblin"]
+let isWaiting = false
 
 function getNewMonster() {
     const nextMonsterData = characterData[monstersArray.shift()]
@@ -17,46 +10,49 @@ function getNewMonster() {
 }
 
 function attack() {
-    wizard.getDiceHtml()
-    monster.getDiceHtml()
-    wizard.takeDamage(monster.currentDiceScore)
-    monster.takeDamage(wizard.currentDiceScore)
-    render()
-    
+    if(!isWaiting){
+        wizard.setDiceHtml()
+        monster.setDiceHtml()
+        wizard.takeDamage(monster.currentDiceScore)
+        monster.takeDamage(wizard.currentDiceScore)
+        render()
+        
         if(wizard.dead){
-            setTimeout(() => endGame(), 1500)
-        } else if (monster.dead) {
-            document.getElementById("attack-button").disabled = true
-				if(monstersArray.length > 0) {
-					setTimeout(() => {
-						monster = getNewMonster() 
-                        document.getElementById("attack-button").disabled = false
-						render()
-					}, 1000)
-					
-				} else {
-					endGame()
-				}
-		} 
+            endGame()
+        }
+        else if(monster.dead){
+            isWaiting = true
+            if(monstersArray.length > 0){
+                setTimeout(()=>{
+                    monster = getNewMonster()
+                    render()
+                    isWaiting = false
+                },1500)
+            }
+            else{
+                endGame()
+            }
+        }    
+    }
 }
 
 function endGame() {
+    isWaiting = true
     const endMessage = wizard.health === 0 && monster.health === 0 ?
         "No victors - all creatures are dead" :
         wizard.health > 0 ? "The Wizard Wins" :
             "The monsters are Victorious"
 
     const endEmoji = wizard.health > 0 ? "🔮" : "☠️"
-    setTimeout(() => {
-        document.body.innerHTML = `
-        <div class="end-game">
-            <h2>Game Over</h2> 
-            <h3>${endMessage}</h3>
-            <p class="end-emoji">${endEmoji}</p>
-        </div>
-        `
-    }, 1500)
-    
+        setTimeout(()=>{
+            document.body.innerHTML = `
+                <div class="end-game">
+                    <h2>Game Over</h2> 
+                    <h3>${endMessage}</h3>
+                    <p class="end-emoji">${endEmoji}</p>
+                </div>
+                `
+        }, 1500)
 }
 
 document.getElementById("attack-button").addEventListener('click', attack)
